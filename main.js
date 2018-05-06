ce46929e49efe2635f51e89f329d4befdad8db5f
Andrew Brady <92andrew.brady@gmail.com>
2018-05-06 15:45:15 -0400

    Add basic HTML and javascript files
    The index file is very basic only the quote, author and button are present. The main.js file is pretty much finished, it contains the javascript and jQuery that pairs with the random quote API and is functionally sound.

---
+++ main.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+	function getNewQuote() {
+		$.ajax({
+			url: 'http://api.forismatic.com/api/1.0/',
+			jsonp: 'jsonp',
+			dataType: 'jsonp',
+			data: {
+				method: 'getQuote',
+				lang: 'en',
+				format: 'jsonp'
+			},
+			success: function(response) {
+				$('#quote').text(response.quoteText);
+				if (response.quoteAuthor) {
+					$('#author').text(response.quoteAuthor);
+				}	else {
+					$('#author').text('Unknown');
+				}
+			}
+		});
+	};
+	getNewQuote();
+	
+	$('#submit').on('click', function() {
+		getNewQuote();
+	});
+});
